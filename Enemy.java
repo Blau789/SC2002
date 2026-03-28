eb7fcd1b6e60c5e5e6aafe5c4be6778afda7bfb9
HeZhiqing <he_zhi_qing@outlook.com>
2026-03-28 17:12:41 +0800

    Add Enemy class

---
+++ Enemy.java
@@ -0,0 +1,30 @@
+import java.util.List;
+
+//Enemy is an abstract class inherited from Combatant class and will be the super class of wolf and goblin
+public abstract class Enemy extends Combatant {
+    protected EnemyActionStrategy actionStrategy;
+
+    public Enemy(String name, int maxHp, int attack, int defense, int speed, EnemyActionStrategy actionStrategy) {
+        super(name, maxHp, attack, defense, speed);
+        this.actionStrategy = actionStrategy;
+    }
+
+
+    public EnemyActionStrategy getActionStrategy() {
+        return actionStrategy;
+    }
+
+    public Action decideAction(List<Combatant> targets) {
+        if (actionStrategy == null) {
+            return null;
+        }
+        return actionStrategy.decideAction(this, targets);
+    }
+
+    public Combatant selectTarget(List<Combatant> targets) {
+        if (actionStrategy == null) {
+            return null;
+        }
+        return actionStrategy.selectTarget(targets);
+    }
+}
