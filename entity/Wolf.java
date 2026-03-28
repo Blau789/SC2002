c7cf3ca14747f07b91c77267b7baff2dfc32d43c
Lau Jun Lit Boris <borislaujl@gmail.com>
2026-03-28 18:18:29 +0800

    Update Wolf.java

--- entity/Wolf.java
+++ entity/Wolf.java
@@ -2,7 +2,7 @@ package entity;
 
 public class Wolf extends Enemy {
     public Wolf(EnemyActionStrategy strategy) {
-        super("entity.Wolf", 40, 45, 5, 35);
+        super("entity.Wolf", 40, 45, 5, 35, strategy);
 
     }
 
