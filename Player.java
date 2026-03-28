46340253944c57e13db1faa27894e3f73f9d4c97
HeZhiqing <he_zhi_qing@outlook.com>
2026-03-28 16:01:30 +0800

    Add Player abstract class with item and cooldown logic

---
+++ Player.java
@@ -0,0 +1,63 @@
+import java.util.ArrayList;
+import java.util.List;
+
+//Player is an abstract class inherited from Combatant class and will be the super class of warrior and wizard
+public abstract class Player extends Combatant {
+    protected List<Item> items;
+    protected int specialSkillCooldown;
+
+    public Player(String name, int maxHp, int attack, int defense, int speed) {
+        super(name, maxHp, attack, defense, speed);
+        this.items = new ArrayList<>();
+        this.specialSkillCooldown = 0;
+    }
+
+    public List<Item> getItems() {
+        return items;
+    }
+
+    public boolean hasItems() {
+        return !items.isEmpty();
+    }
+
+    public void addItem(Item item) {
+        if (item != null) {
+            items.add(item);
+        }
+    }
+
+    public void removeItem(Item item) {
+        items.remove(item);
+    }
+
+    public boolean isSpecialSkillReady() {
+        return specialSkillCooldown == 0;
+    }
+
+    public void resetCooldown() {
+        specialSkillCooldown = 3;
+    }
+
+    public void decrementCooldown() {
+        if (specialSkillCooldown > 0) {
+            specialSkillCooldown--;
+        }
+    }
+
+    public int getCooldown() {
+        return specialSkillCooldown;
+    }
+
+    //for cases when the users use power stone (cooldown = 0)
+    public void setCooldown(int cd) {
+        if (cd < 0) {
+            specialSkillCooldown = 0;
+        } else {
+            specialSkillCooldown = cd;
+        }
+    }
+
+    public abstract String useSpecialSkill(List<Combatant> targets);
+
+    public abstract String getSpecialSkillName();
+}
