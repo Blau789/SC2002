d7ce90b4c4222e672d66537fa3a6f449c61844ad
Lau Jun Lit Boris <borislaujl@gmail.com>
2026-03-28 18:19:07 +0800

    Update Goblin.java

--- entity/Goblin.java
+++ entity/Goblin.java
@@ -3,7 +3,7 @@ package entity;
 public class Goblin extends Enemy {
 
     public Goblin(EnemyActionStrategy strategy) {
-        super("entity.Goblin", 55, 35, 15, 25, strategy);
+        super("Goblin", 55, 35, 15, 25, strategy);
     }
 
     public Goblin(String name, EnemyActionStrategy strategy) {
