b6db1600fabefcbb2ef5f7115ca4c42f0c367e1f
chayapolk22 <chayapolofficial06@gmail.com>
2026-03-29 16:36:33 +0800

    optimize potion implemetation

--- entity/items/Potion.java
+++ entity/items/Potion.java
@@ -7,11 +7,13 @@ public class Potion implements Item{
 	private static final int HEAL_AMOUNT = 100;
 
 	@Override
-	public String use(Combatant user, List<Combatant> enemies) {
-		int newHp = Math.min(user.getHp() + HEAL_AMOUNT, user.getMaxHp());
-		user.setHp(newHp);
-		return user.getName() + " used entity.items.Potion and healed to " + user.getHp() + " HP.";
-	}
+    public String use(Combatant user, List<Combatant> allEnemies) {
+        int oldHp = user.getHp();
+        user.heal(HEAL_AMOUNT);
+        int healed = user.getHp() - oldHp;
+        return user.getName() + " uses Potion! HP: " + oldHp + " → " + user.getHp()
+                + " (+" + healed + ")";
+    }
 
 	@Override
 	public String getName() {
