b27ee948fff0c74182946367c79cd31d41b49333
Joeongtengkiat <128236149+Joeongtengkiat@users.noreply.github.com>
2026-04-03 17:58:24 +0800

    edited it such that the wizard class now is standalone

--- entity/combatant/Wizard.java
+++ entity/combatant/Wizard.java
@@ -2,27 +2,11 @@ package SC2002.entity.combatant;
 
 import SC2002.Action.ArcaneBlastAction;
 public class Wizard extends Player {
-    private int bonusAttack;
 
     public Wizard(String name) {
         super(name, 200, 50, 10, 20);
-        this.bonusAttack = 0;
         this.specialSkill = new ArcaneBlastAction(); 
     }
 
-    @Override
-    public int getAttack() {
-        return attack + bonusAttack;
-    }
-
-    public void addBonusAttack(int amount) {
-        if (amount > 0) {
-            bonusAttack += amount;
-        }
-    }
-
-    public int getBonusAttack() {
-        return bonusAttack;
-    }
 }
 
