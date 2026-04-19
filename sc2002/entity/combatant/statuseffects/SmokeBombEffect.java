9626e3c6e738d2cfe3c8196a437ae93b9f95c368
Joeongtengkiat <128236149+Joeongtengkiat@users.noreply.github.com>
2026-04-19 13:09:59 +0800

    deleted extra text

--- sc2002/entity/combatant/statuseffects/SmokeBombEffect.java
+++ sc2002/entity/combatant/statuseffects/SmokeBombEffect.java
@@ -11,7 +11,6 @@ public class SmokeBombEffect implements StatusEffect {
 
     @Override
     public void apply(Combatant target) {
-        // Effect is checked externally by StatusEffectManager
     }
 
     @Override
